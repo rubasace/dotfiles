03dc82cde428ab8f156a1080b68dbdc9a49ef4d0
rubenpve <rverdugo@travix.com>
2022-02-13 02:41:59 +0100

    Disable autosuggestions plugin

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -1,4 +1,4 @@
 export ZSH_PLUGINS_DIR="${HOME}/zsh-plugins"
 
-source "${ZSH_PLUGINS_DIR}/zsh-autosuggestions/zsh-autosuggestions.zsh"
+#source "${ZSH_PLUGINS_DIR}/zsh-autosuggestions/zsh-autosuggestions.zsh"
 source "${ZSH_PLUGINS_DIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
