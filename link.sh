155e1616aa6d27d8e9d7547cb80426f934c8255d
rubenpve <rverdugo@travix.com>
2022-02-13 01:28:24 +0100

    Add logic to symlink everythin

---
+++ link.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_CONFIG_DIR="${SCRIPT_DIR}/config"
+
+find "${REPO_CONFIG_DIR}" -mindepth 1 -maxdepth 1 -type f -exec sh -c 'ln -sf {} "${HOME}/$(basename {})"' \;
+
+find "${REPO_CONFIG_DIR}" -mindepth 1 -maxdepth 1 -type d -exec sh -c 'ln -sf {} "${HOME}/.config/$(basename {})"' \;
