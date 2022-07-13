04a598418b73e022d9a429d8aee1d935eb81ea45
Ruben Pahino Verdugo <rubasace@MacBook-Pro.local>
2022-07-13 23:37:07 +0200

    Add more things to install

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 # TODO: add tools installation
+
+brew install --cask iterm2
+brew install --cask google-chrome
 brew install lsd
 brew install fzf
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 brew install kubectl
 mkdir -p $HOME/zsh-plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-plugins/zsh-syntax-highlighting
+brew tap homebrew/cask-fonts
+brew install --cask font-meslo-lg-nerd-font
