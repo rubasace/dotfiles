35ac6887f992ccefc7c02eb6fffe98542df87e61
rubasace <rubasodin18@gmail.com>
2023-12-27 02:42:44 +0100

    Add wireguard

--- .install-macos-personal.sh
+++ .install-macos-personal.sh
@@ -1,6 +1,7 @@
 echo "Installing Mac OS personal packages..."
 
 brew install kubeseal
+brew install wireguard-tools
 brew install mas
 
 brew install --cask karabiner-elements
@@ -13,6 +14,8 @@ brew install --cask microsoft-remote-desktop
 mas install 937984704
 # Magnet
 #mas install 441258766
+# Wireguard
+mas install 1451685025
 
 
 echo "Done"
