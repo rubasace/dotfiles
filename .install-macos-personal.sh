beac1ef1ef84e4f0c61dc25bf99156289efe012d
rubasace <rubasodin18@gmail.com>
2024-10-08 18:34:53 +0200

    Make brew autoupdate

--- .install-macos-personal.sh
+++ .install-macos-personal.sh
@@ -1,5 +1,10 @@
 echo "Installing Mac OS personal packages..."
 
+brew tap domt4/autoupdate
+# Update every 4 hours
+brew install pinentry-mac
+/opt/homebrew/bin/brew autoupdate start 14400 --upgrade --cleanup --immediate --sudo
+
 brew install kubeseal
 brew install wireguard-tools
 brew install mas
